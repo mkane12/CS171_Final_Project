103d5143f50153409742485c1d65d6c7382d9d61
Morgan McDaniel <morganemcdaniel@gmail.com>
2016-11-30 10:58:10 -0500

    barchart

--- js/taxRevenue.js
+++ js/taxRevenue.js
@@ -54,6 +54,9 @@ TaxRevenue.prototype.initVis = function() {
     // Invoke the tip in the context of your visualization
     vis.svg.call(vis.tip);
 
+    vis.stack = d3.layout.stack()
+        .values(function(d) {return d.values});
+
     vis.wrangleData();
 };
 
@@ -80,18 +83,22 @@ TaxRevenue.prototype.wrangleData = function() {
         return a.total - b.total;
     });
 
-    vis.name = ["actual", "projection"];
+    vis.legendData = [{name: "Actual Hotel Revenue", color: "#F16664"},
+                        {name: "Projected Airbnb Revenue", color: "#79CCCD"},
+                        {name: "Actual Expenditures", color: "#FFF6E6"}]
 
-    vis.transposedData = vis.name.map(function(name) {
-       return {
-           name: name,
-           values: vis.filteredData.map(function(d) {
-               return {dept: d.dept, y: d[name]};
-           })
-       }
-    });
+    // vis.transposedData = vis.name.map(function(name) {
+    //    return {
+    //        name: name,
+    //        values: vis.filteredData.map(function(d) {
+    //            return {dept: d.dept, y: d[name]};
+    //        })
+    //    }
+    // });
+    //
+    // vis.stackedData = vis.stack(vis.transposedData);
 
-    vis.displayData = vis.transposedData;
+    vis.displayData = vis.filteredData;
 
     // Update the visualization
     vis.updateVis();
@@ -107,46 +114,18 @@ TaxRevenue.prototype.updateVis = function() {
 
     var vis = this;
 
-    var stack = d3.layout.stack()
-        .values(function(d) {return d.values});
+    vis.barHeight = vis.height / vis.displayData.length;
 
-    vis.stackedData = stack(vis.transposedData);
+    // vis.x.domain([0, d3.max(vis.displayData, function(d) {
+    //     return d3.max(d.values, function (e) {
+    //         return e.y0 + e.y;
+    //         });
+    //     })
+    // ]);
 
-    // vis.stackedData = vis.stackedData.map(function (group) {
-    //     return group.map(function (d) {
-    //         return {
-    //             x: d.y,
-    //             y: d.dept,
-    //             x0: d.y0
-    //         };
-    //     });
-    // });
-
-    vis.barHeight = vis.height/vis.stackedData.length;
-
-    vis.x.domain([0, d3.max(vis.stackedData, function(d) {
-        return d3.max(d.values, function (e) {
-            return e.y0 + e.y;
-            });
-        })
-    ]);
-
-    console.log(vis.x.domain());
-
-    // vis.stackData = vis.displayData.filter(function(d) {
-    //     return (d.dept == "Hotel Tax Revenue");
-    // });
-
-    // vis.stackIndex = 0;
-
-
-    // for (i = 0; i < vis.displayData.length; i++) {
-    //     if (vis.displayData[i].dept == "Hotel Tax Revenue") {
-    //         vis.stackIndex = i;
-    //     }
-    // }
-
-    console.log(vis.stackedData);
+    vis.x.domain([0, d3.max(vis.displayData, function (d) {
+        return d.total;
+    })]);
 
     vis.xAxis = d3.svg.axis()
         .scale(vis.x)
@@ -172,54 +151,16 @@ TaxRevenue.prototype.updateVis = function() {
         .text("Budget Line Items")
         .attr("class", "vis-title");
 
-    vis.tip.html(function(d) {
-        return formatCurrency(d.x.toLocaleString());
+    vis.tip.html(function (d) {
+        return formatCurrency(d.actual.toLocaleString());
     });
 
-    vis.bars = vis.svg.selectAll("rect")
-        .data(vis.stackedData);
-
-    vis.bars
-        .enter()
-        .append("rect")
-        .attr("class", "bar");
-
-    vis.bars
-        .transition()
-        .duration(800)
-        .attr("x", function(d) { return vis.x(d.y0); })
-        .attr("y", function(d, index) {
-            return (index * vis.barHeight);
-        })
-        .attr("height", vis.barHeight - 3)
-        .attr("width", function(d) { return vis.x(d.y); })
-        .attr("fill", function(d) {
-            if (d.values.dept == "Hotel Tax Revenue") {
-                return "#F16664";
-            }
-            else {
-                return "#FFF6E6";
-            }
-        })
-    ;
-
-    vis.bars
-        .on("mouseover", function(d) {
-            d3.select(this)
-                .attr("opacity", .5);
-            vis.tip.show(d);
-        })
-        .on("mouseout", function(d) {
-            d3.select(this)
-                .attr("opacity", 1);
-            vis.tip.hide(d);
-        })
-    ;
-
-    vis.bars.exit().remove();
+    /*
+     * Stacked bar chart using d3.stack layout - haven't been able to get it to work yet, but it's the best option if I can fix it.
+     */
 
     // vis.bars = vis.svg.selectAll("rect")
-    //     .data(vis.displayData);
+    //     .data(vis.stackedData);
     //
     // vis.bars
     //     .enter()
@@ -229,20 +170,20 @@ TaxRevenue.prototype.updateVis = function() {
     // vis.bars
     //     .transition()
     //     .duration(800)
+    //     .attr("x", function(d) { return vis.x(d.y0); })
+    //     .attr("y", function(d, index) {
+    //         return (index * vis.barHeight);
+    //     })
+    //     .attr("height", vis.barHeight - 3)
+    //     .attr("width", function(d) { return vis.x(d.y); })
     //     .attr("fill", function(d) {
-    //         if (d.dept == "Hotel Tax Revenue") {
+    //         if (d.values.dept == "Hotel Tax Revenue") {
     //             return "#F16664";
     //         }
     //         else {
     //             return "#FFF6E6";
     //         }
     //     })
-    //     .attr("x", 0)
-    //     .attr("y", function(d, index) {
-    //         return (index * vis.barHeight);
-    //     })
-    //     .attr("height", vis.barHeight - 3)
-    //     .attr("width", function(d) { return vis.x(d.actual); })
     // ;
     //
     // vis.bars
@@ -259,44 +200,110 @@ TaxRevenue.prototype.updateVis = function() {
     // ;
     //
     // vis.bars.exit().remove();
-    //
-    // vis.stack = vis.svg.selectAll(".stack")
-    //     .data(vis.stackData);
-    //
-    // vis.stack
-    //     .enter()
-    //     .append("rect")
-    //     .attr("class", "stack");
-    //
-    // vis.stack
-    //     .transition()
-    //     .duration(800)
-    //     .attr("fill", "#79CCCD")
-    //     .attr("x", function(d) { return vis.x(d.actual); })
-    //     .attr("y", function() {
-    //         return (vis.stackIndex * vis.barHeight);
-    //     })
-    //     .attr("height", vis.barHeight - 3)
-    //     .attr("width", function(d) { return vis.x(d.projection); })
-    // ;
-    //
-    // vis.stack
-    //     .on("mouseover", function(d) {
-    //         d3.select(this)
-    //             .attr("opacity", .5);
-    //         vis.tip.show(d);
-    //     })
-    //     .on("mouseout", function(d) {
-    //         d3.select(this)
-    //             .attr("opacity", 1);
-    //         vis.tip.hide(d);
-    //     })
-    // ;
-    //
-    // vis.stack.exit().remove();
+
+
+    /*
+     * Stacked bar chart without using d3.stack layout - Issues with tooltips
+     */
+
+    vis.stackData = vis.displayData.filter(function (d) {
+        return (d.dept == "Hotel Tax Revenue");
+    });
+
+    vis.stackIndex = 0;
+
+
+    for (i = 0; i < vis.displayData.length; i++) {
+        if (vis.displayData[i].dept == "Hotel Tax Revenue") {
+            vis.stackIndex = i;
+        }
+    }
+
+    vis.bars = vis.svg.selectAll("rect")
+        .data(vis.displayData);
+
+    vis.bars
+        .enter()
+        .append("rect")
+        .attr("class", "bar");
+
+    vis.bars
+        .transition()
+        .duration(800)
+        .attr("fill", function (d) {
+            if (d.dept == "Hotel Tax Revenue") {
+                return "#F16664";
+            }
+            else {
+                return "#FFF6E6";
+            }
+        })
+        .attr("x", 0)
+        .attr("y", function (d, index) {
+            return (index * vis.barHeight);
+        })
+        .attr("height", vis.barHeight - 3)
+        .attr("width", function (d) {
+            return vis.x(d.actual);
+        })
+    ;
+
+    vis.bars
+        .on("mouseover", function (d) {
+            d3.select(this)
+                .attr("opacity", .5);
+            vis.tip.show(d);
+        })
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .attr("opacity", 1);
+            vis.tip.hide(d);
+        })
+    ;
+
+    vis.bars.exit().remove();
+
+    vis.stack = vis.svg.selectAll(".stack")
+        .data(vis.stackData);
+
+    vis.stack
+        .enter()
+        .append("rect")
+        .attr("class", "stack");
+
+    vis.stack
+        .transition()
+        .duration(800)
+        .attr("fill", "#79CCCD")
+        .attr("x", function (d) {
+            return vis.x(d.actual);
+        })
+        .attr("y", function () {
+            return (vis.stackIndex * vis.barHeight);
+        })
+        .attr("height", vis.barHeight - 3)
+        .attr("width", function (d) {
+            return vis.x(d.projection);
+        })
+    ;
+
+    vis.stack
+        .on("mouseover", function (d) {
+            d3.select(this)
+                .attr("opacity", .5);
+            vis.tip.show(d);
+        })
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .attr("opacity", 1);
+            vis.tip.hide(d);
+        })
+    ;
+
+    vis.stack.exit().remove();
 
     vis.labels = vis.svg.selectAll(".text")
-        .data(vis.stackedData);
+        .data(vis.displayData);
 
     vis.labels
         .enter()
@@ -305,14 +312,48 @@ TaxRevenue.prototype.updateVis = function() {
 
     vis.labels
         .attr("x", -10)
-        .attr("y", function(d, index) {
-            return (index * vis.barHeight + (vis.barHeight + 3)/2);
+        .attr("y", function (d, index) {
+            return (index * vis.barHeight + (vis.barHeight + 3) / 2);
         })
         .style("text-anchor", "end")
-        .text(function(d) { return d.dept; });
+        .text(function (d) {
+            return d.dept;
+        });
 
     vis.labels.exit().remove();
-};
+
+
+    // DRAW LEGEND
+
+    vis.svg.selectAll(".legendEntry").remove();
+
+    // append legend
+
+    vis.legend = vis.svg.selectAll('g.legendEntry')
+        .data(vis.legendData)
+        .enter().append('g')
+        .attr('class', 'legendEntry');
+
+    vis.legend
+        .append('rect')
+        .attr("x", vis.width - 200)
+        .attr("y", function (d, i) {
+            return i * 20 + 50;
+        })
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("stroke", "none")
+        .style("stroke-width", 1)
+        .style("fill", function (d) { return d.color; });
+
+    vis.legend
+        .append('text')
+        .attr("x", vis.width - 180)
+        .attr("y", function (d, i) {
+            return i * 20 + 60;
+        })
+        .text(function (d) { return d.name;});
+}
 
 function kFormatter(num) {
     return '$' + (num/1000000) + 'M';
@@ -329,4 +370,4 @@ TaxRevenue.prototype.changeData = function() {
     vis.yearValue = d3.select("#budgetYear").property("value");
 
     vis.wrangleData();
-};
+}
